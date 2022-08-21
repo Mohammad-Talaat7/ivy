139d274302106471cee63d1c61a3d870d70cd33f
Mohammad-Talaat7 <110403191+Mohammad-Talaat7@users.noreply.github.com>
2022-08-21 15:56:13 +0200

    Update averages_and_variances.py
    add test_numpy_var function

--- ivy/functional/frontends/numpy/statistics/averages_and_variances.py
+++ ivy/functional/frontends/numpy/statistics/averages_and_variances.py
@@ -1,54 +1,183 @@
 # global
+import numpy as np
+from hypothesis import given, strategies as st
+
+# local
+import ivy_tests.test_ivy.helpers as helpers
+import ivy.functional.backends.numpy as ivy_np
+import ivy_tests.test_ivy.test_frontends.test_numpy.helpers as np_frontend_helpers
 import ivy
-from ivy.func_wrapper import from_zero_dim_arrays_to_float
 
 
-@from_zero_dim_arrays_to_float
-def mean(
-    x,
-    /,
-    *,
-    axis=None,
-    keepdims=False,
-    out=None,
-    dtype=None,
-    where=True,
+@st.composite
+def statistical_dtype_values(draw, *, function):
+    dtype = draw(st.sampled_from(ivy_np.valid_float_dtypes))
+
+    size = draw(st.integers(1, 10))
+
+    if dtype == "float16":
+        max_value = 2048
+    elif dtype == "float32":
+        max_value = 16777216
+    elif dtype == "float64":
+        max_value = 9.0071993e15
+
+    if function == "prod":
+        abs_value_limit = 0.99 * max_value ** (1 / size)
+    elif function in ["var", "std"]:
+        abs_value_limit = 0.99 * (max_value / size) ** 0.5
+    else:
+        abs_value_limit = 0.99 * max_value / size
+
+    values = draw(
+        helpers.list_of_length(
+            x=st.floats(
+                -abs_value_limit,
+                abs_value_limit,
+                allow_subnormal=False,
+                allow_infinity=False,
+            ),
+            length=size,
+        )
+    )
+
+    shape = np.asarray(values, dtype=dtype).shape
+    size = np.asarray(values, dtype=dtype).size
+    axis = draw(helpers.get_axis(shape=shape, allow_none=True))
+    if function == "var" or function == "std":
+        if isinstance(axis, int):
+            correction = draw(st.integers(-shape[axis], shape[axis] - 1)
+                              | st.floats(-shape[axis], shape[axis] - 1))
+            return dtype, values, axis, correction
+
+        correction = draw(st.integers(-size, size - 1)
+                          | st.floats(-size, size - 1))
+        return dtype, values, axis, correction
+
+    return dtype, values, axis
+
+
+# mean
+@given(
+    dtype_and_x=statistical_dtype_values(function="mean"),
+    dtype=st.sampled_from(ivy_np.valid_float_dtypes + (None,)),
+    where=np_frontend_helpers.where(),
+    as_variable=helpers.array_bools(num_arrays=1),
+    with_out=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.mean"
+    ),
+    native_array=helpers.array_bools(num_arrays=1),
+    keep_dims=st.booleans()
+)
+def test_numpy_mean(
+    dtype_and_x,
+    dtype,
+    where,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    fw,
+    keep_dims,
 ):
-    axis = tuple(axis) if isinstance(axis, list) else axis
-    if dtype:
-        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    input_dtype, x, axis = dtype_and_x
+    x_array = ivy.array(x)
 
-    ret = ivy.mean(x, axis=axis, keepdims=keepdims, out=out)
-    if ivy.is_array(where):
-        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    if len(x_array.shape) == 2:
+        where = ivy.ones((x_array.shape[0], 1), dtype=ivy.bool)
+    elif len(x_array.shape) == 1:
+        where = True
 
-    return ret
+    if isinstance(axis, tuple):
+        axis = axis[0]
+
+    input_dtype = [input_dtype]
+    where = np_frontend_helpers.handle_where_and_array_bools(
+        where=where,
+        input_dtype=input_dtype,
+        as_variable=as_variable,
+        native_array=native_array,
+    )
+
+    np_frontend_helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="numpy",
+        fn_tree="mean",
+        x=np.asarray(x, dtype=input_dtype[0]),
+        axis=axis,
+        dtype=dtype,
+        out=None,
+        keepdims=keep_dims,
+        where=where,
+        test_values=False,
+    )
 
 
-mean.unsupported_dtypes = {"torch": ("float16",)}
-
-
-@from_zero_dim_arrays_to_float
-def var(
-    x,
-    /,
-    *,
-    axis=None,
-    ddof=0.0,
-    keepdims=False,
-    out=None,
-    dtype=None,
-    where=True,
+# var
+@given(
+    dtype_and_x=statistical_dtype_values(function="var"),
+    dtype=st.sampled_from(ivy_np.valid_float_dtypes + (None,)),
+    where=np_frontend_helpers.where(),
+    as_variable=helpers.array_bools(num_arrays=1),
+    with_out=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.var"
+    ),
+    native_array=helpers.array_bools(num_arrays=1),
+    keep_dims=st.booleans(),
+)
+def test_numpy_var(
+    dtype_and_x,
+    dtype,
+    where,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    fw,
+    ddof,
+    keep_dims,
 ):
-    axis = tuple(axis) if isinstance(axis, list) else axis
-    if dtype:
-        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    input_dtype, x, axis = dtype_and_x
+    x_array = ivy.array(x)
 
-    ret = ivy.var(x, axis=axis, ddof=ddof, keepdims=keepdims, out=out)
-    if ivy.is_array(where):
-        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    if len(x_array.shape) == 2:
+        where = ivy.ones((x_array.shape[0], 1), dtype=ivy.bool)
+    elif len(x_array.shape) == 1:
+        where = True
 
-    return ret
+    if isinstance(axis, tuple):
+        axis = axis[0]
 
+    input_dtype = [input_dtype]
+    where = np_frontend_helpers.handle_where_and_array_bools(
+        where=where,
+        input_dtype=input_dtype,
+        as_variable=as_variable,
+        native_array=native_array,
+    )
 
-var.unsupported_dtypes = {"torch": ("float16",)}
+    np_frontend_helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="numpy",
+        fn_tree="var",
+        x=np.asarray(x, dtype=input_dtype[0]),
+        axis=axis,
+        dtype=dtype,
+        out=None,
+        ddof=0,
+        keepdims=keep_dims,
+        where=where,
+        test_values=False,
+    )
